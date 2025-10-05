dc18f87d20d4458594b13c8238b533dbd8f0e5a6
Jan Helcl <janhelcl@gmail.com>
2025-10-05 16:31:25 +0200

    specify date

--- src/domain/prompts/statement_disambiguation.py
+++ src/domain/prompts/statement_disambiguation.py
@@ -2,7 +2,9 @@ statement_disambiguation_instructions = """
 Přečti přiložené PDF. 
 
 Identifikuj, zda obsahuje Rozvahu a Výkaz zisku a ztráty. Může obsahovat obojí.
-Dále identifikuj datum za ke kterému jsou výkazy vydány.
+Dále identifikuj datum ke kterému jsou výkazy vydány.
+
+POZOR: nepleť si datum ke kterému jsou výkazy vydány s datumem kdy jsou zveřejněny. Tyto pojmy jsou odlišné.
 
 Vracíš pouze json, nic jiného.
 Formát:
